b665ccab2afe6a8d9bb54bab82dde4f53235f6a0
Subhrodip Mohanta <subhrodipmohanta@gmail.com>
2020-01-11 15:52:25 +0530

    Update Dog.java

--- Java/Dog.java
+++ Java/Dog.java
@@ -1,5 +1,5 @@
-public class Dog(){
-    public static void main(string[] args){
+public class Dog()  {
+    public static void main(string[] args)  {
     	Dog dog1 = new dog();
     	dog1.breed="German Shedpherd";
     	dog1.size=50;
